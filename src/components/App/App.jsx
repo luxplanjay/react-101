59aeef736f4368d8731c0180c0503eccfb718e79
Alexander Repeta <alexander.repeta@gmail.com>
2024-05-07 18:08:31 +0300

    prepare examples

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,9 @@
-import ClickTracker from "../ClickTracker/ClickTracker";
-import { useState } from "react";
-import Timer from "../Timer/Timer";
-import Reader from "../Reader/Reader";
 import css from "./App.module.css";
-import articles from "../../articles.json";
 
 export default function App() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <div className={css.container}>
-      <h1>Effects in React</h1>
-
-      <ClickTracker />
-      <hr />
-
-      <button onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? "Close" : "Open"}
-      </button>
-      {isOpen && <Timer />}
-      <hr />
-
-      <Reader articles={articles} />
+      <h1>Forms in React</h1>
     </div>
   );
 }
