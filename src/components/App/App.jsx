6ac97add930d63556ca44c72b746522fdb76245a
Alexander Repeta <alexander.repeta@gmail.com>
2024-05-21 14:29:33 +0300

    prepare examples

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+import css from "./App.module.css";
+
+export default function App() {
+  return (
+    <div className={css.container}>
+      <h1>Routing in React</h1>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import App from "./components/App/App";
 import "modern-normalize";
-import LangProvider from "./lang-ctx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LangProvider>
-      <App />
-    </LangProvider>
+    <App />
   </React.StrictMode>
 );
